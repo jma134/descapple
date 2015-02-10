c8b75804d897c5d7154162e0ae432911b99786d6
samma <sam.ma@dhl.com>
2015-02-10 17:33:40 +0800

    12th Commit, added soldto
    Signed-off-by: samma <sam.ma@dhl.com>

--- sam_modules/transport/models/partner.py
+++ sam_modules/transport/models/partner.py
@@ -8,3 +8,5 @@ class partner(models.Model):
     # Add a new column to the res.partner model, by default partners are not
     # instructors
     instructor = fields.Boolean("Instructor", default=False)
+    soldtopt = fields.Integer("Sold-to pt", default=0)
+    shiptopt = fields.Integer("Ship-to pt", default=0)
