b6c9faa65aaade1a79be99c366177ae9c38cdd49
samma <sam.ma@dhl.com>
2015-03-31 17:12:04 +0800

    35th commit, access control adjustment
    Signed-off-by: samma <sam.ma@dhl.com>

--- sam_modules/springback/models/product.py
+++ sam_modules/springback/models/product.py
@@ -2,7 +2,11 @@
 
 from openerp import fields, models
 
+class product_product(models.Model):
+    _inherit = "product.product"    
+    
 class product_template(models.Model):
+    _name = 'product.template'
     _inherit = 'product.template'
 
     vol_per_plt = fields.Integer("Volume per Pallet", default=1)
